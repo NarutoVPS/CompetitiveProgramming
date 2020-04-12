3d3929713926e20e060aaf99f488e0da6b6eadfa
Naruto <singhvedprakash075@gmail.com>
2020-04-12 21:09:06 +0530

    Improved Algorithm

--- CPP/UtopianTree[HackerRank].cpp
+++ CPP/UtopianTree[HackerRank].cpp
@@ -6,19 +6,17 @@ int main(){
     int T;
     cin>>T;
 
-    int N{},height{};
+    int N{},height{1};
     while(T){
         cin>>N;
-        for(int i{0};i<=N;i++){
-            if(i==0)
-                height = 1;
-            else if(i%2==0)
+        for(int i{1};i<=N;i++){
+            if(i%2==0)
                 height += 1;
             else
                 height *= 2;  
         }
         cout<<height<<endl;
-        height = 0;
+        height = 1;
         T--;
     }
 }
