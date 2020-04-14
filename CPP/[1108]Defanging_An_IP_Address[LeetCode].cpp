afebe896c59bbdfa0d3749c72e31520a46ba32a6
Naruto <singhvedprakash075@gmail.com>
2020-04-15 03:58:02 +0530

    [1108]Defanging_An_IP_Address[LeetCode]

---
+++ CPP/[1108]Defanging_An_IP_Address[LeetCode].cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string from {"."};
+        string to {"[.]"};
+        int pos{};
+        while((pos = address.find(from, pos)) != string::npos){
+            address.replace(pos, 1, to);
+            pos += to.length();
+        }
+        return address;
+    }
+};
+
+int main(){
+    Solution s;
+    cout<<s.defangIPaddr("192.168.1.1");
+}
