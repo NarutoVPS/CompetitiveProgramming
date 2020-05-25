12bbfbcdf51fa74732e57974dbd1779eaa672542
Naruto <singhvedprakash075@gmail.com>
2020-05-25 21:19:32 +0530

    added question

--- CPP/IncreasingArray.cpp
+++ CPP/IncreasingArray.cpp
@@ -1,3 +1,28 @@
+/*
+	You are given an array of n integers. 
+	You want to modify the array so that it is increasing, i.e., every element is at least as large as the previous element.
+	On each turn, you may increase the value of any element by one. What is the minimum number of turns required?
+
+	Input
+		The first input line contains an integer n: the size of the array.
+		Then, the second line contains n integers x1,x2,…,xn: the contents of the array.
+
+	Output
+		Print the minimum number of turns.
+
+	Constraints
+		1≤n≤2⋅105
+		1≤xi≤109
+
+	Example
+		Input:
+			5
+			3 2 5 1 7
+
+		Output:
+			5
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -18,10 +43,10 @@ long long solve(vector<long long> num){
 }
 
 int main(){
-	#ifndef NARUTO
-	freopen ("../input.txt", "r", stdin);
-	freopen ("../output.txt", "w", stdout);
-	#endif
+	// #ifndef NARUTO
+	// freopen ("../input.txt", "r", stdin);
+	// freopen ("../output.txt", "w", stdout);
+	// #endif
 
 	int n;
 	cin>>n;
