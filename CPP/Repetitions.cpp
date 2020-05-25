d4d799babd595a6c54429a28278227791aeb02dd
Naruto <singhvedprakash075@gmail.com>
2020-05-25 20:37:30 +0530

    added question

--- CPP/Repetitions.cpp
+++ CPP/Repetitions.cpp
@@ -1,3 +1,25 @@
+/*
+	You are given a DNA sequence: a string consisting of characters A, C, G, and T. 
+	Your task is to find the longest repetition in the sequence. 
+	This is a maximum-length substring containing only one type of character.
+
+	Input
+		The only input line contains a string of n characters.
+
+	Output
+		Print one integer: the length of the longest repetition.
+
+	Constraints
+		1≤n≤106
+
+	Example
+		Input:
+			ATTCGGGA
+
+		Output:
+			3
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
