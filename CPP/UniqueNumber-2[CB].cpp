5581708206f0ad21eded1ad01663485dd0cf5fae
Naruto <singhvedprakash075@gmail.com>
2020-07-28 17:31:06 +0530

    added question

--- CPP/UniqueNumber-2[CB].cpp
+++ CPP/UniqueNumber-2[CB].cpp
@@ -1,6 +1,30 @@
+/*
+    We are given an array containg n numbers. All the numbers are present twice except for two numbers which are present only once. Find the unique numbers in linear time without using any extra space. ( Hint - Use Bitwise )
+
+    Input Format
+        First line contains the number n. Second line contains n space separated number.
+
+    Constraints
+        n < 10^5
+
+    Output Format
+        Output a single line containing the unique numbers separated by space
+
+    Sample Input
+        4
+        3 1 2 1
+
+    Sample Output
+        2 3
+
+    Explanation
+        Smaller number comes before larger number
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// O(n) Time | O(1) Space
 pair<int, int> solution(vector<int> nums) {
     int xor_{};
     for (int &num : nums)
