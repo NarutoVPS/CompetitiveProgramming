75f3245ae3916c5e188353783bd0790677fe341a
Ved Prakash SIngh <singhvedprakash075@gmail.com>
2020-08-09 20:44:45 +0530

    added question & TSC

--- CPP/DivisibleSubArrays[CB].cpp
+++ CPP/DivisibleSubArrays[CB].cpp
@@ -1,8 +1,39 @@
+/*
+    You are given N elements, a1,a2,a3….aN. Find the number of good sub-arrays.
+    A good sub-array is a sub-array [ai,ai+1,ai+2….aj] such that (ai+ai+1+ai+2+….+aj) is divisible by N.
+
+    Input Format
+        The first line contains the number of test cases T. First line of each test case contains an integer N denoting the number of elements. Second line of each test case contains N integers, a1, a2, a3….aN, where ai denotes the ith element of the array.
+
+    Constraints
+        1<=T<=10
+        1<=N<=10^5
+        |ai|<=10^9
+
+    Output Format
+        Output a single integer denoting the number of good sub-arrays.
+
+    Sample Input
+        2
+        5
+        1 1 1 1 1
+        5
+        5 5 5 5 5
+        
+    Sample Output
+        1
+        15
+
+    Explanation
+        In first test case, there is only one sub-array [1, 1, 1, 1, 1], such that 1+1+1+1+1=5, which is divisible by N=5
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
 
+// O(n) Time || O(n) Space
 int main() {
     ll t;
     cin>>t;
