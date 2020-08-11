557e6d5e84a33ce244990fc1600f11102a40910c
Ved Prakash Singh <singhvedprakash075@gmail.com>
2020-08-11 19:47:44 +0530

    added question

--- CPP/AlmostPrime[CodeForces].cpp
+++ CPP/AlmostPrime[CodeForces].cpp
@@ -1,3 +1,13 @@
+/*
+    A number is called almost prime if it has exactly two distinct prime divisors. For example, numbers 6, 18, 24 are almost prime, while 4, 8, 9, 42 are not. Find the amount of almost prime numbers which are between 1 and n, inclusive.
+
+    Input
+        Input contains one integer number n (1 ≤ n ≤ 3000).
+
+    Output
+        Output the amount of almost prime numbers between 1 and n, inclusive.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
